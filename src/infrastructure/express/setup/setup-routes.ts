7ab74f48e61406ecb4c2c76e7ef789a3de2c98ab
Lucas Andres Marsell <designsdevnicaragua@gmail.com>
2022-09-29 20:59:41 -0600

    :bricks: :necktie: implement api response handler

--- src/infrastructure/express/setup/setup-routes.ts
+++ src/infrastructure/express/setup/setup-routes.ts
@@ -1,8 +1,14 @@
 import { Application } from 'express'
+import { ApiResponse } from '../adapters/api-response-adapter'
 import { ApiType } from '../api'
 
 export const setupRoutes = (app: Application, routes: ApiType[]): void => {
   routes.forEach(route => {
     app.use(route.path, route.controller)
+  })
+
+  app.use((_req, _res, next) => {
+    const error = new Error('Internal server error')
+    next(ApiResponse.internalServerErrorRequets(error.message, {}))
   })
 }
